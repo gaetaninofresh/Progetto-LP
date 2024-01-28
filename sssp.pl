b83efbc1d13f1ae1c3e4b108d8fccf73181294e0
gaetaninofresh <157909249+gaetaninofresh@users.noreply.github.com>
2024-01-28 19:02:46 +0100

    Update sssp.pl

--- sssp.pl
+++ sssp.pl
@@ -21,3 +21,20 @@ new_vertex(G, V) :-
     atom(V),
     not(vertex(G, V)),
     assert(vertex(G, V)).
+
+% vertices(G, Vs) - ritorna ver/falso se Vs è la lista di tutti i vertici di G 
+
+vertices(G, Vs) :-
+    setof(V, vertex(G,V), Vs), !.
+
+vertices(G, Vs) :-
+    is_list(Vs),
+    setof(V, vertex(G,V), L),
+    permutation(L, Vs), !.
+
+% list_vertices(G) - stampa tutti i vertici del grafo G
+    %TODO: rimuovere stampa ":- dynamic vertex/2"
+
+list_vertices(G) :-
+    graph(G),
+    listing(vertex(G, V)).
