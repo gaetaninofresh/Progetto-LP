648626a6c2e660d6037601dcd05fc0ae1d84e34c
ChernoBillv2 <larocca.gaetano10@gmail.com>
2024-01-29 13:09:35 +0100

    Update sssp.pl

--- sssp.pl
+++ sssp.pl
@@ -1,6 +1,7 @@
 % per evitare warning
 :- dynamic graph/1.
 :- dynamic vertex/2.
+:- dynamic edge/4.
 
 % new_graph(G) - aggiunge il grafo G alla base di conoscenza
 
@@ -38,3 +39,44 @@ vertices(G, Vs) :-
 list_vertices(G) :-
     graph(G),
     listing(vertex(G, V)).
+<<<<<<< Updated upstream
+=======
+
+    
+
+% new_edge(G, U, V, Weight) - Aggiunge un arco del grafo G 
+%   alla base dati Prolog.
+new_edge(G, U, V, Weight) :-
+    assert(edge(G, U, V, Weight)).
+
+new_edge(G, U, V) :-
+    new_edge(G, U, V, 1).
+
+% edges(G, Es) - Questo predicato è vero quando Es è una lista di tutti
+%   gli archi presenti in G.
+
+edges(G, Es) :-
+    setof(V, vertex(G,V), Es), !.
+
+edges(G, Es) :-
+    is_list(Es),
+    setof(V, vertex(G,V), L),
+    permutation(L, Es), !.
+
+% neighbors(G, V, Ns) - Questo predicato è vero quando V è un vertice di G e 
+%   Ns è una lista contenente gli archi, edge(G, V, N, W), 
+%   che portano ai vertici N immediatamente raggiungibili da V 
+
+% TODO: the rock controlla la funzione
+
+neighbors(G, V, Ns) :-
+    findall(N, edge(G, V, N, _), Ns).
+    % passo _ perchè il peso dei nodi non mi interssa
+
+% list_edges(G) - Questo predicato stampa alla console dell’interprete Prolog
+%   una lista degli archi del grafo G
+
+list_edges(G) :-
+    graph(G),
+    listing(edge(G, U, V, Weight)).
+>>>>>>> Stashed changes
